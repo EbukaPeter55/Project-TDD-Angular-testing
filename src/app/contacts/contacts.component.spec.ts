2c0c61d02b42a3d7a85379d565d1aea50b75dc8c
Ebuka Peter <peterdbrainy5@gmail.com>
2021-09-12 01:06:50 +0100

    Added some unit test for contactComponent

--- src/app/contacts/contacts.component.spec.ts
+++ src/app/contacts/contacts.component.spec.ts
@@ -5,4 +5,30 @@ import { Contact } from "./shared/models";
 
 describe ('ContactsComponent Tests', ()=> {
 let contactsComponent: ContactsComponent = null;
+
+beforeEach(()=>{
+    contactsComponent = new ContactsComponent()
+    });
+//    Test whether an instance of ContactsComponent can be created properly
+    it('should set instance correctly', ()=>{
+        expect(contactsComponent).not.toBeNull(); //Assertion where I set whether the component is set correctly
+    });
+    // Test to make sure number of contacts array length is 0 
+    it('should be no contacts if there is no data', () => {
+        expect(contactsComponent.contacts.length).toBe(0);
+    });
+    // Test to ensure new contact can be added to the list 
+    it('Should be able to add new contact to the list where there is data', () => {
+        const newContact = {
+            id: 1,
+            name: 'Ebuka'
+        }
+        const contactsList: Array<Contact> = [newContact];
+        contactsComponent.contacts = contactsList;
+        
+        expect(contactsComponent.contacts.length ).toBe(1);
+
+    });
 });
+
+
